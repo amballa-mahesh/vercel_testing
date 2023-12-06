a3188fab16c801334723612f35ffd62c19a5c749
amballa-mahesh <amballa.mahesh89@gmail.com>
2023-12-07 00:59:02 +0530

    for app

---
+++ api/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, redirect, url_for,render_template,request
+import matplotlib.pyplot as plt
+import os
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+headers = {"Authorization": "Bearer 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"}
+
+def query_image(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome_user():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods = ['POST','GET'])
+def submit():
+    back = request.referrer
+    if request.method == 'POST':
+        img  = request.files['img'] 
+        img = plt.imread(img)
+        path = os.path.join('static/images/img_new.jpg')        
+        plt.imsave(path,img)        
+        output = query_image(path)
+        caption = output[0]['generated_text']      
+        print(caption)  
+        return render_template('index.html',img_path = path,result = caption)
+    
+    return redirect(back)
+
+
+
+if __name__== '__main__':
+    app.run(debug=True)
